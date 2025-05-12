c8e12d62a6eb257ff51557596cbc05617f5a5d22
mikikito <mikikito@ip-10-95-1-240.ap-northeast-1.compute.internal>
2025-05-12 15:53:27 +0900

    tes

---
+++ src/tes/heloworid.java
@@ -0,0 +1,15 @@
+package tes;
+
+public class heloworid {
+	  public static void main(String[] args) 
+	  {
+		System.out.println("Hello World!");
+		String name ="たろう";
+		int a =4;
+		int b =6;
+		int kotae =4*6;
+		System.out.println(name);
+		System.out.println(kotae);
+	  }
+	  
+}
